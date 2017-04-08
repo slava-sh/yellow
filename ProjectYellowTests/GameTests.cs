61300362b9b4eda8d25d10279e2b25c2ce43ed78
Slava Shklyaev <slava@slava.sh>
2017-04-08 12:30:16 +0200

    Add tests

---
+++ ProjectYellowTests/GameTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectYellow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectYellow.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        [TestMethod()]
+        public void NewGameIsNotOver()
+        {
+            var game = new Game(randomSeed: 0);
+            Assert.IsFalse(game.IsOver);
+        }
+    }
+}
