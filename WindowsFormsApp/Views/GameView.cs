f1dc5944319bc3de9e91ba125247910ade2c1b7c
Slava Shklyaev <slava@slava.sh>
2017-04-15 21:36:51 +0200

    Remove WindowsFromsApp.Utils

--- WindowsFormsApp/Views/GameView.cs
+++ WindowsFormsApp/Views/GameView.cs
@@ -70,7 +70,24 @@ namespace WindowsFormsApp.Views
             }
             var nextTetromino = GetNextTetromino();
             var mask = nextTetromino.GetMaskForRotation(Rotation.Default);
-            DrawMask(Utils.Crop(mask, 4, 2));
+            DrawMask(Crop(mask, 4, 2));
+        }
+
+        private static bool[,] Crop(bool[,] mask, int newWidth, int newHeight)
+        {
+            var newMask = new bool[newWidth, newHeight];
+            var maskWidth = mask.GetLength(0);
+            var maskHeight = mask.GetLength(1);
+            var minWidth = Math.Min(maskWidth, newWidth);
+            var minHeight = Math.Min(maskHeight, newHeight);
+            for (var x = 0; x < minWidth; ++x)
+            {
+                for (var y = 0; y < minHeight; ++y)
+                {
+                    newMask[x, y] = mask[x, y];
+                }
+            }
+            return newMask;
         }
 
         private void DrawStats(Statistics stats)
